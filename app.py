c16d6c3a4978b69c6b6ba3764b1b173da0e53951
idiotblin <arsenavh@gmail.com>
2020-12-08 16:57:36 +0300

    task1

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask
+from random import randint
+
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/haba')
 def hello_world():
+    s = '''Hello, Haba!
+           Hello, Arsen!
+           Hello, Karim!'''
+    out = "<pre>{}</pre>".format(s)
+    return out
 
-    s = ["Hello, Haba!",
-        "Hello, Arsen!",
-        "Hello, Karim!"]
 
-    out = "<pre>{}</pre>".format("\n".join(s))
+@app.route('/task1/random')
+def solve1():
+    r = randint(1, 5)
+    s = "Haba's mark is " + str(r)
+    out = "<pre>{}</pre>".format(s)
     return out
