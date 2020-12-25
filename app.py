a97a75f9037828538d36fa05a942fd66249c9347
idiotblin <arsenavh@gmail.com>
2020-12-25 22:02:41 +0300

    task3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
 from random import randint
-import requests, json
+
+import json
+import requests
+from flask import Flask, render_template, abort, redirect, request
 
 app = Flask(__name__)
 
@@ -92,3 +94,57 @@ def task2_num(num):
     else:
         res = {"status": "FAIL"}
     return f'{json.dumps(res)}'
+
+
+@app.route('/task3/cf/profile/<handle>/')
+def task3_cf_profile_no_page(handle):
+    return redirect(f'/task3/cf/profile/{handle}/page/1')
+
+
+@app.route('/task3/cf/profile/<handle>/page/<int:page_number>/')
+def task3_cf_profile(handle, page_number):
+    data = json.loads(requests.get(f'https://codeforces.com/api/user.status?handle={handle}&from=1&count=100').text)
+    if data['status'] != 'OK':
+        abort(404)
+    problem = list()
+    problem.append(list())
+    ind = 0
+    for attemp in data['result']:
+        problem[ind].append((attemp['creationTimeSeconds'], attemp['problem']['name'], attemp['verdict']))
+        if len(problem[ind]) == 25:
+            ind += 1
+            problem.append(list())
+    if not 0 < page_number <= len(problem):
+        abort(404)
+    if len(problem) == 5:
+        problem.pop()
+    link = list()
+    link.append(('Previous', f'/task3/cf/profile/{handle}/page/{page_number - 1}/'))
+    for i in range(len(problem)):
+        link.append((i + 1, f'/task3/cf/profile/{handle}/page/{str(i + 1)}/'))
+    link.append(('Next', f'/task3/cf/profile/{handle}/page/{page_number + 1}/'))
+    return render_template('pagination.html', problem=problem, page=page_number, link=link, page_kol=len(problem))
+
+
+@app.route('/task3/cf/top/')
+def top():
+    data = request.args
+    url = 'https://codeforces.com/api/user.info?handles={}'
+    ans = list()
+    try:
+        orderby = data['orderby']
+    except:
+        orderby = "None"
+    for i in data['handles'].split('|'):
+        ans.append([f'/task3/cf/profile/{i}/', i, requests.get(url.format(i)).json()['result'][0]['rating']])
+    y, reverse = (-2, False) if orderby == "None" else (-1, True)
+    ans.sort(key=lambda x: x[y], reverse=reverse)
+    return render_template("top.html", ans=ans)
+
+
+@app.errorhandler(404)
+def cf_error_page(error):
+    return render_template("404_not_found.html"), 404
+
+
+app.run()
