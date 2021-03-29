7de9e10ab06ae281a47ae6a00fdd9bee4afa2413
idiotblin <75247753+idiotblin@users.noreply.github.com>
2021-03-29 18:10:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import smtplib
 
 app = Flask(__name__)
 
+app.secret_key = bytes(os.environ['secret'])
+
 conn = psycopg2.connect(
     f'dbname={os.environ["Dbname"]} user={os.environ["User"]} password={os.environ["Password"]} host={os.environ["Host"]} port={os.environ["Port"]}')
 cur = conn.cursor()
