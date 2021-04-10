a79f889bbea4603b2d158218769d818d8235f7f1
idiotblin <75247753+idiotblin@users.noreply.github.com>
2021-04-10 09:57:09 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-engine = create_engine(os.environ['DATABASE_URL'])
+engine = create_engine(os.environ['DATABASE_URL'].replace("postgres", "postgresql", 1))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
